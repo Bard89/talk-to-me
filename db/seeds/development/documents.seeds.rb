58ecec174c14987e05c31f81cf29d80f2b6261d1
Voi <vojtech.matous@gmail.com>
2024-04-21 23:16:09 +0300

    Seed more documents for every user

--- db/seeds/development/documents.seeds.rb
+++ db/seeds/development/documents.seeds.rb
@@ -3,12 +3,14 @@
 after 'development:users' do
   notify __FILE__
 
-  User.find_each.with_index do |user, index|
-    seed Document,
-         { title: "Document title #{index}"} ,
-         { document_type: %w[pdf text link].sample,
-           content: FFaker::Lorem.paragraph,
-           status: Document::STATUS.sample,
-           user: user }
+  User.find_each.with_index do |user|
+    5.times do |index|
+      seed Document,
+           { title: "Document title #{index}"} ,
+           { document_type: %w[pdf text link].sample,
+             content: FFaker::Lorem.paragraph,
+             status: Document::STATUS.sample,
+             user: user }
+    end
   end
 end
