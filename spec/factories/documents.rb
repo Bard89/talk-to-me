708fc4e2d6ec8d3673ebb580c2dc645728f96791
Voi <vojtech.matous@gmail.com>
2024-08-26 02:46:55 +0200

    Add annotate ails task to override disfunctiona default, annotate everything

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: documents
+#
+#  id            :bigint           not null, primary key
+#  content       :text
+#  document_type :string
+#  status        :string
+#  title         :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#
+# Indexes
+#
+#  index_documents_on_user_id  (user_id)
+#
 FactoryBot.define do
   factory :document do
     title { FFaker::Lorem.sentence }
--- spec/models/voice_spec.rb
+++ spec/models/voice_spec.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: voices
+#
+#  id          :bigint           not null, primary key
+#  audio       :binary
+#  status      :string
+#  voice_type  :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  document_id :bigint           not null
+#
+# Indexes
+#
+#  index_voices_on_document_id  (document_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (document_id => documents.id)
+#
 require "rails_helper"
 
 RSpec.describe Voice do
