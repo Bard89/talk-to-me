9e1052ea8ce543750866eb0059c5b3c948716d66
Voi <vojtech.matous@gmail.com>
2024-09-02 00:44:14 +0200

    Fix n + 1s

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,7 +5,7 @@ class DocumentsController < ApplicationController
   before_action :set_document, only: %i[show edit update destroy]
 
   def index
-    @documents = current_user.documents.order(created_at: :desc)
+    @documents = current_user.documents.includes(:voices).order(created_at: :desc)
   end
 
   def show; end
